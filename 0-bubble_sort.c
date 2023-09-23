74ca3c96fe64d7635566875874b1f32a022d0695
Aziz <ngwk.zz@outlook.com>
2023-09-23 02:21:55 +0000

    Bubble sort.
    Co-authored-by: Khaoula <khaoula.nakkach@gmail.com>

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -26,29 +26,29 @@ void swap_ints(int *a, int *b)
 
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, len = size;
-
-	bool track_swap = false;
+	size_t i, j;
+	bool swapped;
 
 	if (array == NULL || size < 2)
 		return;
 
-	while (track_swap == false)
+	for (i = 0; i < size - 1; i++)
 	{
-		track_swap = true;
+		swapped = false;
 		/* Iterate through the array from the first, second ... to */
 		 /* the last element */
-		for (i = 0; i < (len - 1); i++)
+		for (j = 0; j < (size - i - 1); j++)
 		{
-			if (array[i] > array[i + 1])
+			if (array[j] > array[j + 1])
 			{
-				swap_ints(array + i, (array + i + 1));
+				swap_ints(&array[j], &array[j + 1]);
 				/* Prints the array after each swap */
 				print_array(array, size);
-				track_swap = false;
+				swapped = true;
 			}
 		}
-		len--;
+		if (!swapped)
+			break;
 	}
 }
 
