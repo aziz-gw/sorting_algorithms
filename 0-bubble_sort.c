21af9b5403bfe20a71a78db446d15603c92972d8
Aziz <ngwk.zz@outlook.com>
2023-09-23 02:21:55 +0000

    Bubble sort.
    Co-authored-by: Khaoula <khaoula.nakkach@gmail.com>

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include "sort.h"
+#include <stdbool.h>
 
 /**
  * swap_ints - swaps two integers in an array.
@@ -26,25 +26,30 @@ void swap_ints(int *a, int *b)
 
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j, k;
+	size_t i, len;
 
-	/* Iterate through the array from the first, second ... to the */
-	/* last element */
-	for (i = 0; i < size - 1; i++)
+	bool track_swap = false;
+
+	if (array == NULL || size < 2)
+		return;
+
+	while (track_swap == false)
 	{
-		/* Iterates through the unsorted portion of the array */
-		for (j = 0; j < ((size - i) - 1); j++)
+		track_swap = true;
+		len = size;
+		/* Iterate through the array from the first, second ... to */
+		 /* the last element */
+		for (i = 0; i < (len - 1); i++)
 		{
-			if (array[j] > array[j + 1])
+			if (array[i] > array[i + 1])
 			{
-				swap_ints(&array[j], &array[j + 1]);
-
+				swap_ints(array + i, ((array + i) + 1));
 				/* Prints the array after each swap */
-				for (k = 0; k < size; k++)
-					printf("%d ", array[k]);
-				printf("\n");
+				print_array(array, size);
+				track_swap = false;
 			}
 		}
+		len--;
 	}
 }
 
