1a6fba8e6dd73f4ece945176a3dc423146f42c86
khaoula1972 <khaoula.nakkach@gmail.com>
2023-09-25 01:49:33 +0100

    heap sort
    Co-authored-by: aziz-gw <ngwk.zz@outlook.com>

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -2,11 +2,11 @@
 #include "sort.h"
 
 /**
- * swap - Function to swap two elements in an array
+ * swap - swapfunc
  * @a: a pointer
  * @b: a pointer
  */
-void swap(int *a, int *b)
+void swap(int *a,int *b)
 {
 	int temp = *a;
 
@@ -18,24 +18,26 @@ void swap(int *a, int *b)
  * heapify - Function to heapify a subtree rooted at given index
  * @arr: an array
  * @size: size of the array
+ * @t_be: the base
  * @root: the root
  */
-void heapify(int arr[], size_t size, size_t root)
+void heapify(int arr[], size_t size, size_t t_be, size_t root)
 {
 	size_t largest = root;
 	size_t left = 2 * root + 1;
 	size_t right = 2 * root + 2;
 
-	if (left < size && arr[left] > arr[largest])
+	if (left < t_be && arr[left] > arr[largest])
 		largest = left;
 
-	if (right < size && arr[right] > arr[largest])
+	if (right < t_be && arr[right] > arr[largest])
 		largest = right;
 
 	if (largest != root)
 	{
 		swap(&arr[root], &arr[largest]);
-		heapify(arr, size, largest);
+		print_array(arr, size);
+		heapify(arr, size, t_be, largest);
 	}
 }
 
@@ -49,12 +51,18 @@ void heap_sort(int *array, size_t size)
 {
 	int i;
 
-	for (i = size / 2 - 1; i >= 0; i--)
-		heapify(array, size, i);
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = (size / 2) - 1; i >= 0; i--)
+	{
+		heapify(array, size, size, i);
+	}
 
 	for (i = size - 1; i > 0; i--)
 	{
 		swap(&array[0], &array[i]);
-		heapify(array, i, 0);
+		print_array(array, size);
+		heapify(array, size, i, 0);
 	}
 }
